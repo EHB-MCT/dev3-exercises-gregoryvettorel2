6099dd8928ce8453471f15e11749c239a715a993
Gregory Vettorel <43611326+gregoryvettorel2@users.noreply.github.com>
2021-10-07 16:38:15 +0200

    Tot en met 1ste extra gemaakt

---
+++ Session1/Exercise2(rockpaperscissors)/src/main/kotlin/Main.kt
@@ -0,0 +1,54 @@
+fun main() {
+    val options = listOf("Rock", "Paper", "Scissors")
+
+    //Picks a random
+    val c = options.random()
+
+    //Takes user input
+    val u = getInput()
+
+    val x = validateInput(u)
+
+    if(x == "") {
+        println("You picked $u")
+        println("Computer picked $c")
+        println(play(c, u))
+    } else {
+        println(x)
+        return
+    }
+}
+
+fun getInput():String? {
+    println("Enter Rock, Paper or Scissors:")
+    val userInput = readLine()
+    return userInput
+}
+
+fun play(computerInput:String?,userInput:String?): String {
+    if(computerInput == userInput){
+        return "It's a tie!"
+    } else if(computerInput == "Rock" && userInput == "Scissors"){
+        return "Computer wins! You lose!"
+    } else if(computerInput == "Rock" && userInput == "Paper"){
+        return "You win! Computer loses!"
+    } else if (computerInput == "Scissors" && userInput == "Paper"){
+        return "Computer wins! You lose!"
+    } else if (computerInput == "Scissors" && userInput == "Rock") {
+        return "You win! Computer loses!"
+    } else if (computerInput == "Paper" && userInput == "Scissors"){
+        return "You win! Computer loses!"
+    } else if (computerInput == "Paper" && userInput == "Rock") {
+        return "Computer wins! You lose!"
+    } else {
+        return "error"
+    }
+}
+
+fun validateInput(userInput: String?): String {
+    if(userInput == "Rock" || userInput == "Scissors" || userInput == "Paper"){
+        return ""
+    } else {
+        return "Please enter a valid option."
+    }
+}
