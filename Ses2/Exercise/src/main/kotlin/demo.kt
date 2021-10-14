aa4f18f5a2b44288471f28f15a0914f722a09736
Gregory Vettorel <43611326+gregoryvettorel2@users.noreply.github.com>
2021-10-14 16:01:14 +0200

    challengeTwo

--- Ses2/Exercise/src/main/kotlin/demo.kt
+++ Ses2/Exercise/src/main/kotlin/demo.kt
@@ -3,7 +3,10 @@ fun main() {
     println("You are an adventurer trying to get a treasure that is hidden deep inside a piramid.")
     println("You must solve layers of riddles to get to the treasure room, good luck....")
     challengeOne()
+    println("The door opens, you pass through the long hallway and at the end a talking cat appears, he says:")
     challengeTwo()
+    println("You passed the second challenge! The cat hisses at you and runs away.")
+    println("You go down a ladder")
 }
 
 //CHALLENGES
@@ -24,9 +27,25 @@ fun challengeOne() {
 }
 
 fun challengeTwo() {
+    println("'For the next challenge you need to guess low (1-3) or high (4-6). I will then roll the dice. If you're right you can pass, but if you're wrong ....'")
+    println("Your number:")
+    val userNumber = readLine()!!.toInt()
 
+    val catNumber = rollDice()
+
+    if(catNumber < 4 && userNumber < 4){
+        win()
+    } else if (catNumber > 3 && userNumber > 3) {
+        win()
+    } else {
+        gameOver()
+    }
 }
 
+fun rollDice():Int {
+    val randomNumber = (1..6).random()
+    return randomNumber
+}
 
 //WIN OR LOSE FUNCTIONS
 fun win() {
